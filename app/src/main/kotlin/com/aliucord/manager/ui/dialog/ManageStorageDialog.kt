564370ab9c6df9bd06965816439e767e7caa0855
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-09-23 16:54:06 -0700

    fix: using `isExternalStorageManager` without api check (#22)

--- app/src/main/kotlin/com/aliucord/manager/ui/dialog/ManageStorageDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/dialog/ManageStorageDialog.kt
@@ -6,18 +6,21 @@ import android.os.Environment
 import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.window.DialogProperties
 import androidx.core.net.toUri
+import com.aliucord.manager.BuildConfig
 import com.aliucord.manager.R
 
+@RequiresApi(Build.VERSION_CODES.R)
 @Composable
 fun ManageStorageDialog() {
     var manageStorageGranted by remember { mutableStateOf(Environment.isExternalStorageManager()) }
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !manageStorageGranted) {
+    if (!manageStorageGranted) {
         AlertDialog(
             onDismissRequest = {},
             confirmButton = {
@@ -29,7 +32,7 @@ fun ManageStorageDialog() {
                 Button(
                     onClick = {
                         Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-                            .setData("package:com.aliucord.manager".toUri())
+                            .setData("package:${BuildConfig.APPLICATION_ID}".toUri())
                             .let { launcher.launch(it) }
                     },
                 ) {
