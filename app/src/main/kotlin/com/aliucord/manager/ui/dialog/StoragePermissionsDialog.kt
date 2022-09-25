c1495737701e36ca2053d45beb83211869c87401
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2022-09-24 19:22:53 -0700

    refactor: separate into 3 components

--- app/src/main/kotlin/com/aliucord/manager/ui/dialog/StoragePermissionsDialog.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/dialog/StoragePermissionsDialog.kt
@@ -16,59 +16,69 @@ import com.aliucord.manager.BuildConfig
 import com.aliucord.manager.R
 import com.google.accompanist.permissions.*
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun StoragePermissionsDialog() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        var manageStorageGranted by remember { mutableStateOf(Environment.isExternalStorageManager()) }
-
-        if (!manageStorageGranted) {
-            AlertDialog(
-                onDismissRequest = {},
-                confirmButton = {
-                    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                        if (Environment.isExternalStorageManager())
-                            manageStorageGranted = true
-                    }
-
-                    Button(
-                        onClick = {
-                            Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-                                .setData("package:${BuildConfig.APPLICATION_ID}".toUri())
-                                .let { launcher.launch(it) }
-                        },
-                    ) {
-                        Text(stringResource(R.string.open_settings))
-                    }
-                },
-                title = { Text(stringResource(R.string.permissions_grant)) },
-                text = { Text(stringResource(R.string.permissions_manage_storage_body)) },
-                properties = DialogProperties(
-                    dismissOnBackPress = false,
-                    dismissOnClickOutside = false
-                )
-            )
-        }
+        ManageStorageDialog()
     } else {
-        val writeStorageState = rememberPermissionState(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ExternalStorageDialog()
+    }
+}
 
-        if (!writeStorageState.status.isGranted) {
-            AlertDialog(
-                onDismissRequest = {},
-                confirmButton = {
-                    Button(
-                        onClick = { writeStorageState.launchPermissionRequest() },
-                    ) {
-                        Text(stringResource(R.string.confirm))
-                    }
-                },
-                title = { Text(stringResource(R.string.permissions_grant)) },
-                text = { Text(stringResource(R.string.permissions_regular_body)) },
-                properties = DialogProperties(
-                    dismissOnBackPress = false,
-                    dismissOnClickOutside = false
-                )
+@Composable
+private fun ManageStorageDialog() {
+    var manageStorageGranted by remember { mutableStateOf(Environment.isExternalStorageManager()) }
+
+    if (!manageStorageGranted) {
+        AlertDialog(
+            onDismissRequest = {},
+            confirmButton = {
+                val launcher = rememberLauncherForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                    if (Environment.isExternalStorageManager())
+                        manageStorageGranted = true
+                }
+
+                Button(
+                    onClick = {
+                        Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+                            .setData("package:${BuildConfig.APPLICATION_ID}".toUri())
+                            .let { launcher.launch(it) }
+                    },
+                ) {
+                    Text(stringResource(R.string.open_settings))
+                }
+            },
+            title = { Text(stringResource(R.string.permissions_grant)) },
+            text = { Text(stringResource(R.string.permissions_grant_body)) },
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
             )
-        }
+        )
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun ExternalStorageDialog() {
+    val writeStorageState = rememberPermissionState(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    if (!writeStorageState.status.isGranted) {
+        AlertDialog(
+            onDismissRequest = {},
+            confirmButton = {
+                Button(
+                    onClick = { writeStorageState.launchPermissionRequest() },
+                ) {
+                    Text(stringResource(R.string.confirm))
+                }
+            },
+            title = { Text(stringResource(R.string.permissions_grant)) },
+            text = { Text(stringResource(R.string.permissions_grant_body)) },
+            properties = DialogProperties(
+                dismissOnBackPress = false,
+                dismissOnClickOutside = false
+            )
+        )
     }
 }
