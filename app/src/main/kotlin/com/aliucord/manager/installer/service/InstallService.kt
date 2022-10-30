f30f3ad9428172d1c09a7569377c36f9d55104e0
Wing <44992537+wingio@users.noreply.github.com>
2022-10-29 22:51:44 -0400

    feat: upgraded installer ui (#34)
    * Feat: Upgraded installer ui
    
    * rusher will perish
    
    * wing will explode
    
    * husj
    
    * husj2
    
    Co-authored-by: wingio <wingio@users.noreply.github.com>
    Co-authored-by: Diamond <33725716+DiamondMiner88@users.noreply.github.com>

--- app/src/main/kotlin/com/aliucord/manager/installer/service/InstallService.kt
+++ app/src/main/kotlin/com/aliucord/manager/installer/service/InstallService.kt
@@ -5,36 +5,26 @@ import android.content.Intent
 import android.content.pm.PackageInstaller
 import android.os.IBinder
 import android.util.Log
-import android.widget.Toast
 import com.aliucord.manager.BuildConfig
 import com.aliucord.manager.R
+import com.aliucord.manager.util.showToast
 
 class InstallService : Service() {
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         when (val statusCode = intent.getIntExtra(PackageInstaller.EXTRA_STATUS, -999)) {
             PackageInstaller.STATUS_PENDING_USER_ACTION -> {
-                @Suppress("DEPRECATION") // No.
-                val confirmationIntent = intent.getParcelableExtra<Intent>(Intent.EXTRA_INTENT)!!
+                val confirmationIntent = intent.getParcelableExtra(Intent.EXTRA_INTENT, Intent::class.java)!!
                     .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
 
                 startActivity(confirmationIntent)
             }
 
-            PackageInstaller.STATUS_SUCCESS -> Toast.makeText(
-                this,
-                getString(R.string.install_success),
-                Toast.LENGTH_LONG
-            ).show()
-
-            PackageInstaller.STATUS_FAILURE_ABORTED -> Toast.makeText(
-                this,
-                getString(R.string.install_aborted),
-                Toast.LENGTH_LONG
-            ).show()
+            PackageInstaller.STATUS_SUCCESS -> showToast(R.string.installer_success)
+            PackageInstaller.STATUS_FAILURE_ABORTED -> showToast(R.string.installer_aborted)
 
             else -> {
                 Log.i(BuildConfig.TAG, "Install failed with error code $statusCode")
-                Toast.makeText(this, getString(R.string.install_failed, statusCode), Toast.LENGTH_LONG).show()
+                showToast(R.string.installer_failed, statusCode)
             }
         }
 
