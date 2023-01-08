22b327f1aeed84ab78489515b3dcc393a650ae8c
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2023-01-08 09:53:46 -0800

    progress flow

--- app/src/main/kotlin/com/aliucord/manager/domain/manager/DownloadManager.kt
+++ app/src/main/kotlin/com/aliucord/manager/domain/manager/DownloadManager.kt
@@ -11,8 +11,10 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.withContext
 import java.io.File
+import kotlin.math.min
 
 class DownloadManager(
     private val preferences: PreferencesManager,
@@ -39,77 +41,94 @@ class DownloadManager(
     }
 
     // Discord APK downloading
-    suspend fun downloadDiscordApk(version: String, out: File): File =
-        download("${getBaseUrl()}/download/discord?v=$version", out)
+    suspend fun downloadDiscordApk(version: String, out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download("${getBaseUrl()}/download/discord?v=$version", out, progress)
 
-    suspend fun downloadSplit(version: String, split: String, out: File): File =
-        download("${getBaseUrl()}/download/discord?v=$version&split=$split", out)
+    suspend fun downloadSplit(version: String, split: String, out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download("${getBaseUrl()}/download/discord?v=$version&split=$split", out, progress)
 
     // Aliucord Kotlin downloads
-    suspend fun downloadKtInjector(out: File): File =
-        download(aliucordGithub.getKtInjectorUrl(), out)
+    suspend fun downloadKtInjector(out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download(aliucordGithub.getKtInjectorUrl(), out, progress)
 
-    suspend fun downloadAliuhook(version: String, out: File): File =
-        download(aliucordMaven.getAliuhookUrl(version), out)
+    suspend fun downloadAliuhook(version: String, out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download(aliucordMaven.getAliuhookUrl(version), out, progress)
 
-    suspend fun downloadKotlinDex(out: File): File =
-        download(aliucordGithub.getKtDexUrl(), out)
+    suspend fun downloadKotlinDex(out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download(aliucordGithub.getKtDexUrl(), out, progress)
 
-    suspend fun downloadBootstrap(out: File): File =
-        download(aliucordGithub.getBootstrapUrl(), out)
+    suspend fun downloadBootstrap(out: File, progress: MutableSharedFlow<Int?>? = null): File =
+        download(aliucordGithub.getBootstrapUrl(), out, progress)
 
-    suspend fun download(url: String, out: File): File {
-        out.parentFile?.mkdirs()
-        val tmpOut = out.resolveSibling("${out.name}.tmp")
-            .apply { exists() || createNewFile() }
-        val outStream = tmpOut.outputStream()
+    /**
+     * Download to a file with Ktor while keeping progress
+     * @param progress A flow that emits download progress (percentage or null when unknown)
+     */
+    suspend fun download(url: String, out: File, progress: MutableSharedFlow<Int?>? = null): File {
+        withContext(Dispatchers.IO) {
+            out.parentFile?.mkdirs()
+            val tmpOut = out.resolveSibling("${out.name}.tmp")
+                .apply { exists() || createNewFile() }
 
-        val newUrl = if (!preferences.httpOnly) url else run {
-            var redirect = url
+            val newUrl = if (!preferences.httpOnly) url else run {
+                var redirect = url
 
-            for (i in 0 until 5) {
-                val resp = httpClient.head {
-                    url(url)
-                    method = HttpMethod.Head
+                for (i in 0 until 5) {
+                    val resp = httpClient.head {
+                        url(url)
+                        method = HttpMethod.Head
+                    }
+
+                    val header = resp.headers[HttpHeaders.Location]
+                    val replaced = header?.replace("https", "http")
+                    println("$redirect $header")
+                    if (header == null) {
+                        return@run redirect
+                    } else if (redirect == replaced) {
+                        throw Error("Failed to download in HTTP only mode, encountered unavoidable redirect to HTTPS")
+                    } else {
+                        redirect = replaced!!
+                    }
                 }
 
-                val header = resp.headers[HttpHeaders.Location]
-                val replaced = header?.replace("https", "http")
-                println("$redirect $header")
-                if (header == null) {
-                    return@run redirect
-                } else if (redirect == replaced) {
-                    throw Error("Failed to download in HTTP only mode, encountered unavoidable redirect to HTTPS")
-                } else {
-                    redirect = replaced!!
-                }
+                throw Error("Reached max limit of redirects while downloading")
             }
 
-            throw Error("Reached max limit of redirects while downloading")
-        }
-
-        withContext(Dispatchers.IO) {
             httpClient.prepareGet {
                 url(newUrl)
                 header(HttpHeaders.UserAgent, "Aliucord Manager/${BuildConfig.VERSION_NAME}")
             }.execute {
                 val channel = it.bodyAsChannel()
+                val total = it.contentLength() ?: 0
+                var retrieved = 0L
 
-                while (!channel.isClosedForRead) {
-                    val packet = channel.readRemaining(1024 * 1024 * 1)
-                    while (!packet.isEmpty) {
-                        val bytes = packet.readBytes()
-                        withContext(Dispatchers.IO) {
-                            outStream.write(bytes)
-                            outStream.flush()
+                tmpOut.outputStream().use { out ->
+                    while (!channel.isClosedForRead) {
+                        val packet = channel.readRemaining(1024 * 1024 * 1)
+                        while (!packet.isEmpty) {
+                            val bytes = packet.readBytes()
+                            withContext(Dispatchers.IO) {
+                                out.write(bytes)
+                                out.flush()
+                            }
+
+                            if (progress != null) {
+                                if (total > 0) {
+                                    retrieved += bytes.size
+                                    progress.emit(min(100, (retrieved * 100 / total).toInt()))
+                                } else {
+                                    progress.emit(null)
+                                }
+                            }
                         }
+                        channel.awaitContent()
                     }
-                    channel.awaitContent()
                 }
             }
+
+            tmpOut.renameTo(out)
         }
 
-        tmpOut.renameTo(out)
         return out
     }
 }
