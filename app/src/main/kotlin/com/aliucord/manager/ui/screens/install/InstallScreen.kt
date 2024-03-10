c75f22beba47d31bd409bf620ddd1ab210e5700d
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-03-10 15:55:23 -0700

    wip

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/install/InstallScreen.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/install/InstallScreen.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.*
@@ -16,6 +17,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -23,13 +28,19 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import com.aliucord.manager.BuildConfig
 import com.aliucord.manager.R
 import com.aliucord.manager.installer.steps.StepGroup
-import com.aliucord.manager.ui.components.Wakelock
-import com.aliucord.manager.ui.components.back
+import com.aliucord.manager.ui.TextBanner
+import com.aliucord.manager.ui.components.*
 import com.aliucord.manager.ui.components.dialogs.InstallerAbortDialog
-import com.aliucord.manager.ui.screens.install.components.*
+import com.aliucord.manager.ui.screens.install.components.InstallAppBar
+import com.aliucord.manager.ui.screens.install.components.StepGroupCard
 import com.aliucord.manager.ui.screens.installopts.InstallOptions
+import com.aliucord.manager.ui.util.paddings.PaddingValuesSides
+import com.aliucord.manager.ui.util.paddings.exclude
+import com.aliucord.manager.util.isIgnoringBatteryOptimizations
+import kotlinx.collections.immutable.persistentListOf
 import org.koin.core.parameter.parametersOf
 
 class InstallScreen(private val data: InstallOptions) : Screen {
@@ -38,8 +49,11 @@ class InstallScreen(private val data: InstallOptions) : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        val context = LocalContext.current
         val model = getScreenModel<InstallModel> { parametersOf(data) }
+
         val state = model.state.collectAsState()
+        val showMinimizationWarning = remember { !context.isIgnoringBatteryOptimizations() }
 
         LaunchedEffect(state.value) {
             if (state.value is InstallScreenState.CloseScreen)
@@ -52,6 +66,9 @@ class InstallScreen(private val data: InstallOptions) : Screen {
         // Exit warning dialog (dismiss itself if install process state changes, esp. for Success)
         var showAbortWarning by remember(model.state.collectAsState()) { mutableStateOf(false) }
 
+        // The currently expanded step group on this screen
+        var expandedGroup by remember { mutableStateOf<StepGroup?>(StepGroup.Prepare) }
+
         // Only show exit warning if currently working
         val onTryExit: () -> Unit = remember {
             {
@@ -61,6 +78,12 @@ class InstallScreen(private val data: InstallOptions) : Screen {
                     navigator.back(currentActivity = null)
                 }
             }
+        }
+
+        // Close all groups when successfully finished everything
+        LaunchedEffect(state.value) {
+            if (state.value == InstallScreenState.Success)
+                expandedGroup = null
         }
 
         if (showAbortWarning) {
@@ -84,43 +107,110 @@ class InstallScreen(private val data: InstallOptions) : Screen {
                     interactionSource = remember(::MutableInteractionSource),
                 ),
         ) { paddingValues ->
-            Column(Modifier.padding(paddingValues)) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(20.dp),
+                modifier = Modifier
+                    .padding(paddingValues.exclude(PaddingValuesSides.Bottom)),
+            ) {
                 if (state.value == InstallScreenState.Working) {
                     LinearProgressIndicator(
                         modifier = Modifier
-                            .fillMaxWidth()
                             .height(4.dp)
+                            .fillMaxWidth()
                     )
                 } else {
                     HorizontalDivider(
                         thickness = 2.dp,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                    )
-
-                    MinimizationWarning(
                         modifier = Modifier.fillMaxWidth(),
                     )
                 }
 
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(20.dp),
+                    contentPadding = paddingValues.exclude(PaddingValuesSides.Horizontal + PaddingValuesSides.Top),
                     modifier = Modifier
-                        .verticalScroll(rememberScrollState())
-                        .fillMaxWidth()
-                        .padding(top = 6.dp)
-                        .padding(16.dp)
+                        .padding(horizontal = 16.dp)
+                        .fillMaxSize(),
                 ) {
-                    var expandedGroup by remember { mutableStateOf<StepGroup?>(StepGroup.Prepare) }
+                    if (showMinimizationWarning && !state.value.isFinished) {
+                        item(key = "MINIMIZATION_WARNING_BANNER") {
+                            Column {
+                                TextBanner(
+                                    text = stringResource(R.string.installer_minimization_warning),
+                                    icon = painterResource(R.drawable.ic_warning),
+                                    iconColor = MaterialTheme.customColors.onWarningContainer,
+                                    outlineColor = MaterialTheme.customColors.warning,
+                                    containerColor = MaterialTheme.customColors.warningContainer,
+                                    modifier = Modifier
+                                        .padding(bottom = 20.dp)
+                                        .fillMaxWidth()
+                                        .animateItemPlacement(),
+                                )
 
-                    // Close all groups when successfully finished everything
-                    LaunchedEffect(state.value) {
-                        if (state.value == InstallScreenState.Success)
-                            expandedGroup = null
+                                HorizontalDivider(
+                                    thickness = (.5).dp,
+                                    modifier = Modifier.fillMaxWidth(),
+                                )
+                            }
+                        }
                     }
 
-                    model.installSteps?.let { groupedSteps ->
-                        for ((group, steps) in groupedSteps.entries) key(group) {
+                    if (state.value is InstallScreenState.Failed) {
+                        stickyHeader(key = "FAILED_BANNER") {
+                            val handler = LocalUriHandler.current
+
+                            Surface(Modifier.animateItemPlacement()) {
+                                Column(
+                                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                                ) {
+                                    TextBanner(
+                                        text = "Installation failed! You can either retry or click this to open the Aliucord server for help.",
+                                        icon = painterResource(R.drawable.ic_warning),
+                                        iconColor = MaterialTheme.colorScheme.error,
+                                        // outlineColor = MaterialTheme.colorScheme.error.copy(alpha = .5f),
+                                        outlineColor = MaterialTheme.colorScheme.errorContainer,
+                                        containerColor = MaterialTheme.colorScheme.errorContainer,
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .clickable { handler.openUri("https://discord.gg/${BuildConfig.SUPPORT_SERVER}") }
+                                    )
+
+                                    FilledTonalIconButton(
+                                        shape = MaterialTheme.shapes.medium,
+                                        colors = IconButtonDefaults.filledTonalIconButtonColors(
+                                            containerColor = MaterialTheme.colorScheme.primary,
+                                        ),
+                                        onClick = model::restart,
+                                        modifier = Modifier.fillMaxWidth(),
+                                    ) {
+                                        Row(
+                                            horizontalArrangement = Arrangement.spacedBy(6.dp),
+                                            verticalAlignment = Alignment.CenterVertically,
+                                        ) {
+                                            Icon(
+                                                painter = painterResource(R.drawable.ic_refresh),
+                                                contentDescription = null,
+                                            )
+                                            Text(
+                                                text = "Retry installation",
+                                                style = MaterialTheme.typography.labelLarge,
+                                            )
+                                        }
+                                    }
+
+                                    HorizontalDivider(
+                                        thickness = (.5).dp,
+                                        modifier = Modifier
+                                            .padding(top = 8.dp, bottom = 10.dp)
+                                            .fillMaxWidth(),
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    for ((group, steps) in model.installSteps?.entries ?: persistentListOf()) {
+                        item(key = System.identityHashCode(group)) {
                             StepGroupCard(
                                 name = stringResource(group.localizedName),
                                 subSteps = steps,
@@ -133,52 +223,59 @@ class InstallScreen(private val data: InstallOptions) : Screen {
                         }
                     }
 
-                    if (state.value.isFinished) {
-                        Row(
-                            horizontalArrangement = Arrangement.End,
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            FilledTonalButton(onClick = model::clearCache) {
-                                Text(stringResource(R.string.setting_clear_cache))
-                            }
-                        }
-                    }
+                    // if (state.value is InstallScreenState.Success) {
+                    //     item(key = "BUTTON_ROW") {
+                    //         Row(
+                    //             horizontalArrangement = Arrangement.End,
+                    //             modifier = Modifier.fillMaxWidth()
+                    //         ) {
+                    //             FilledTonalButton(onClick = model::clearCache) {
+                    //                 Text(stringResource(R.string.setting_clear_cache))
+                    //             }
+                    //         }
+                    //     }
+                    // }
 
                     if (state.value is InstallScreenState.Failed) {
                         val failureLog = (state.value as InstallScreenState.Failed).failureLog
 
-                        Row(
-                            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.End),
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            FilledTonalButton(onClick = model::clearCache) {
-                                Text(stringResource(R.string.setting_clear_cache))
-                            }
+                        item(key = "BUTTON_ROW") {
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.End),
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                FilledTonalButton(onClick = model::clearCache) {
+                                    Text(stringResource(R.string.setting_clear_cache))
+                                }
 
-                            Spacer(Modifier.weight(1f, true))
+                                Spacer(Modifier.weight(1f, true))
 
-                            OutlinedButton(onClick = model::saveFailureLog) {
-                                Text(stringResource(R.string.installer_save_file))
-                            }
+                                OutlinedButton(onClick = model::saveFailureLog) {
+                                    Text(stringResource(R.string.installer_save_file))
+                                }
 
-                            FilledTonalButton(onClick = model::copyDebugToClipboard) {
-                                Text(stringResource(R.string.action_copy))
+                                FilledTonalButton(onClick = model::copyDebugToClipboard) {
+                                    Text(stringResource(R.string.action_copy))
+                                }
                             }
                         }
 
-                        SelectionContainer {
-                            Text(
-                                text = failureLog,
-                                style = MaterialTheme.typography.labelSmall,
-                                fontFamily = FontFamily.Monospace,
-                                softWrap = false,
-                                modifier = Modifier
-                                    .clip(RoundedCornerShape(10.dp))
-                                    .background(MaterialTheme.colorScheme.surfaceColorAtElevation(10.dp))
-                                    .padding(10.dp)
-                                    .horizontalScroll(rememberScrollState())
-                            )
-                        }
+                        // item(key = "ERROR_LOG") {
+                        //     SelectionContainer {
+                        //         Text(
+                        //             text = failureLog,
+                        //             style = MaterialTheme.typography.labelSmall,
+                        //             fontFamily = FontFamily.Monospace,
+                        //             softWrap = false,
+                        //             modifier = Modifier
+                        //                 .padding(top = 20.dp)
+                        //                 .clip(RoundedCornerShape(10.dp))
+                        //                 .background(MaterialTheme.colorScheme.surfaceColorAtElevation(10.dp))
+                        //                 .padding(24.dp)
+                        //                 .horizontalScroll(rememberScrollState())
+                        //         )
+                        //     }
+                        // }
                     }
                 }
             }
