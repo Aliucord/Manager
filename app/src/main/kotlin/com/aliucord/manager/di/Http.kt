9759fbd13be813b36171b4f3fe5c3b78906072ad
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2024-08-01 19:49:25 -0700

    feat(Http): set proper User-Agent

--- app/src/main/kotlin/com/aliucord/manager/di/Http.kt
+++ app/src/main/kotlin/com/aliucord/manager/di/Http.kt
@@ -1,8 +1,12 @@
 package com.aliucord.manager.di
 
+import com.aliucord.manager.BuildConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import okhttp3.Dns
@@ -18,6 +22,9 @@ fun Scope.provideJson() = Json {
 fun Scope.provideHttpClient() = HttpClient(OkHttp) {
     val json: Json = get()
 
+    defaultRequest {
+        header(HttpHeaders.UserAgent, "Aliucord Manager/${BuildConfig.VERSION_NAME}")
+    }
     engine {
         config {
             dns(object : Dns {
