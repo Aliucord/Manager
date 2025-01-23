0fc8a5d6db91a33602aa5f8929b1427ca183c313
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-01-22 18:54:56 -0800

    perf(Patcher): don't compress zip entries that will be aligned later

--- app/src/main/kotlin/com/aliucord/manager/patcher/steps/patch/AddAliuhookLibsStep.kt
+++ app/src/main/kotlin/com/aliucord/manager/patcher/steps/patch/AddAliuhookLibsStep.kt
@@ -8,6 +8,7 @@ import com.aliucord.manager.patcher.steps.base.IDexProvider
 import com.aliucord.manager.patcher.steps.base.Step
 import com.aliucord.manager.patcher.steps.download.CopyDependenciesStep
 import com.aliucord.manager.patcher.steps.download.DownloadAliuhookStep
+import com.github.diamondminer88.zip.ZipCompression
 import com.github.diamondminer88.zip.ZipReader
 import com.github.diamondminer88.zip.ZipWriter
 import org.koin.core.component.KoinComponent
@@ -31,7 +32,7 @@ class AddAliuhookLibsStep : Step(), KoinComponent {
                     val bytes = aliuhook.openEntry("jni/$currentDeviceArch/$libFile")?.read()
                         ?: throw IllegalStateException("Failed to read $libFile from aliuhook aar")
 
-                    patchedApk.writeEntry("lib/$currentDeviceArch/$libFile", bytes)
+                    patchedApk.writeEntry("lib/$currentDeviceArch/$libFile", bytes, ZipCompression.NONE)
                 }
             }
         }
