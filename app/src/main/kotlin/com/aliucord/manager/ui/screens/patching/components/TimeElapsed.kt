59a7ef7dd0ca30306a5a11065bd5bf54e062c62c
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2026-01-05 14:46:31 -0800

    feat(i10n): make time elapsed translatable
    Fixes #130

--- app/src/main/kotlin/com/aliucord/manager/ui/screens/patching/components/TimeElapsed.kt
+++ app/src/main/kotlin/com/aliucord/manager/ui/screens/patching/components/TimeElapsed.kt
@@ -5,6 +5,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.aliucord.manager.R
 
 @Composable
 fun TimeElapsed(
@@ -19,7 +21,7 @@ fun TimeElapsed(
         label = "TimeElapsed Visibility"
     ) {
         Text(
-            text = "%.2fs".format(seconds),
+            text = stringResource(R.string.time_elapsed_seconds, seconds),
             style = MaterialTheme.typography.labelMedium,
             maxLines = 1,
             modifier = modifier,
